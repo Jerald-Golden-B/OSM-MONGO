f0c77512c52de73486f52952a29f9bf2971b6d79
Jerald-Golden-B <jerald@hexrfactory.com>
2024-02-29 10:47:54 +0530

    commit

--- index.js
+++ index.js
@@ -13,49 +13,52 @@ async function run() {
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
     const coordinates = await collection.findOne({
-        geometry: {
-            $geoWithin: {
-                $geometry: {
-                    type: "Polygon",
-                    coordinates: [
-                        [
-                            [-109.99966285694285, 53.465628031282392],
-                            [-109.99943214404341, 48.993082694652713],
-                            [-114.06251701583469, 48.993082694652713],
-                            [-114.73402338910687, 49.587201369660278],
-                            [-114.99279537163449, 50.545033437448694],
-                            [-116.67403322873736, 51.790685340435289],
-                            [-118.22102364419568, 52.386100402211071],
-                            [-118.80367259220323, 53.153396574059229],
-                            [-119.89384048046952, 53.515549908186784],
-                            [-120.00000686635538, 60.001087264965378],
-                            [-110.0000254057848, 60.001087264965378],
-                            [-109.99966285694285, 53.465628031282392]
+        features: [{
+            geometry: {
+                $geoIntersects: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [
+                            -109.99966285694285, 53.465628031282392
                         ]
-                    ]
+                    }
                 }
             }
-        }
+        }]
     })
-
-    const coordinates1 = await collection.findOne({
-        loc: {
-            $geoWithin: {
-                $geometry: {
-                    type: "Polygon",
-                    coordinates: [[
-                        [-73.95, 40.80],
-                        [-73.94, 40.79],
-                        [-73.97, 40.76],
-                        [-73.98, 40.76],
-                        [-73.95, 40.80]
-                    ]]
-                }
-            }
-        }
-    });
     console.log(coordinates);
-    // console.log(coordinates);
+
+    // const coordinates1 = await collection.findOne({
+    //     loc: {
+    //         $geoWithin: {
+    //             $geometry: {
+    //                 type: "Polygon",
+    //                 coordinates: [[
+    //                     [-73.95, 40.80],
+    //                     [-73.94, 40.79],
+    //                     [-73.97, 40.76],
+    //                     [-73.98, 40.76],
+    //                     [-73.95, 40.80]
+    //                 ]]
+    //             }
+    //         }
+    //     }
+    // });
+    // console.log(coordinates1);
+
+    // const coordinates2 = await collection.findOne({
+    //     loc: {
+    //         $geoIntersects: {
+    //             $geometry: {
+    //                 type: "Point",
+    //                 coordinates: [
+    //                     -73.97, 40.77,
+    //                 ]
+    //             }
+    //         }
+    //     }
+    // });
+    // console.log(coordinates2);
 }
 run();
 
