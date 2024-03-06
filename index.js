171d92a9fcae9e030e2a599b75705d57fa774543
Jerald-Golden-B <jerald@hexrfactory.com>
2024-03-06 19:47:16 +0530

    commit

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
